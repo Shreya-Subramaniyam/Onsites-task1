51fa54ae00a61d674298810309771e4560ad7e48
Shreya-Subramaniyam <shreyasubs1950@gmail.com>
2021-08-25 01:03:02 +0530

    third commit

--- stock.js
+++ stock.js
@@ -1,5 +1,5 @@
-// var btn = document.getElementById("btn");
-
+var flag = false;
+var position;
 function read() {
   var data = document.getElementById("name").value;
   return data;
@@ -11,13 +11,47 @@ function insert(data) {
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     cell1.innerHTML = data;
-    cell2.innerHTML = "<a>Edit</a>";
+    cell2.innerHTML = `<a onclick = "edit(this)">Edit</a>
+                      <a onclick = "deleteObj(this)">Delete</a>`;
 }
 
 function submitForm() {
-  console.log("1");
-  var data1 = read();
-  insert(data1);
+  if(flag == false) {
+    var data1 = read();
+    if(data1 != "") {
+      insert(data1);
+      reset();
+    }
+  }
+  else if(flag == true) {
+    var data1 = read();
+    update(data1);
+    reset();
+    flag = false;
+  }
 }
 
-// btn.addEventListener("click", submit());
+function reset() {
+  document.getElementById("name").value = "";
+}
+
+function edit(obj) {
+  // console.log("1");
+  flag = true;
+  var currRow = obj.parentElement.parentElement;
+  document.getElementById("name").value = currRow.cells[0].innerHTML;
+  position = currRow;
+}
+
+function update(obj) {
+  position.cells[0].innerHTML = obj;
+}
+
+function deleteObj(obj) {
+  var currRow = obj.parentElement.parentElement;
+  document.getElementById("List").deleteRow(currRow.rowIndex);
+  reset();
+}
+// function mouseHoverfn(obj) {
+//   obj.style.cursor = pointer;
+// }
